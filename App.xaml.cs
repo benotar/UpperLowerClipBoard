9a82326ec95c105a6f6c0895fca2127aa9918936
Ivan <zhmur.ivan@gmail.com>
2024-05-02 12:59:25 +0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HW_Upper_Lower;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_Upper_Lower;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnConvertBtnClick(object sender, RoutedEventArgs routedEventArgs)
+    {
+        if (String.IsNullOrEmpty(InputTxtBox.Text))
+        {
+            MessageBox.Show("Type the text first!");
+            
+            return;
+        }
+        
+        string text = ((UpperRadioBtn is not null && LowerRadioBtn is not null) && UpperRadioBtn.IsChecked.HasValue
+            && UpperRadioBtn.IsChecked.Value)
+            ? InputTxtBox.Text.ToUpper()
+            : InputTxtBox.Text.ToLower();
+        
+        Clipboard.SetText(text);
+
+        MessageBox.Show("Copied to clipboard!");
+    }
+}
